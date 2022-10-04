6aa054487025c11d5de2c8607530fca781a7d757
Serhii Kupriienko <skupriienko@anaconda.com>
2022-10-04 13:15:03 +0300

    Update to v1.2.4

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,29 +1,41 @@
-md build
-cd build
+:: cmd
+@echo on
 
-:: Hack for winres.h being called winresrc.h on VS2008
-if %VS_MAJOR% LEQ 9 copy %RECIPE_DIR%\winres.h .
+echo "Building %PKG_NAME%."
 
-cmake -LAH -GNinja ..                                               ^
-  -DCMAKE_WINDOWS_EXPORT_ALL_SYMBOLS=ON                             ^
-  -DBUILD_SHARED_LIBS=ON                                            ^
-  -DCMAKE_BUILD_TYPE=Release                                        ^
-  -DCMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX%                           ^
-  -DCMAKE_PREFIX_PATH=%LIBRARY_PREFIX%                              ^
-  -DCMAKE_FIND_ROOT_PATH=%LIBRARY_PREFIX%;%PREFIX%;%BUILD_PREFIX%   ^
-  -DCMAKE_FIND_ROOT_PATH_MODE_LIBRARY=ONLY
-if errorlevel 1 exit 1
+:: Isolate the build.
+mkdir Build-%PKG_NAME%
+cd Build-%PKG_NAME%
+if errorlevel 1 exit /b 1
+
+:: Generate the build files.
+echo "Generating the build files..."
+cmake .. %CMAKE_ARGS% ^
+      -G"Ninja" ^
+      -DCMAKE_BUILD_TYPE=Release ^
+      -DCMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% ^
+      -DCMAKE_WINDOWS_EXPORT_ALL_SYMBOLS=ON ^
+      -DBUILD_SHARED_LIBS:BOOL=ON ^
+      -DWEBP_BUILD_CWEBP:BOOL=OFF ^
+      -DWEBP_BUILD_DWEBP:BOOL=OFF ^
+      -DWEBP_BUILD_EXTRAS:BOOL=OFF ^
+      -DWEBP_BUILD_GIF2WEBP:BOOL=OFF ^
+      -DWEBP_BUILD_IMG2WEBP:BOOL=OFF ^
+      -DWEBP_BUILD_LIBWEBPMUX:BOOL=ON ^
+      -DWEBP_BUILD_VWEBP:BOOL=OFF ^
+      -DWEBP_BUILD_WEBP_JS:BOOL=OFF ^
+      -DWEBP_BUILD_WEBPINFO:BOOL=OFF ^
+      -DWEBP_BUILD_WEBPMUX:BOOL=OFF ^
+      -DWEBP_LINK_STATIC:BOOL=OFF ^
+    ..
+if %ERRORLEVEL% neq 0 exit 1
 
 cmake --build .
-if errorlevel 1 exit 1
-cmake --install .
-if errorlevel 1 exit 1
+if %ERRORLEVEL% neq 0 exit 1
 
-:: for backwards compatibility with previous make build. This will fail on non-ntfs systems.
-pushd %LIBRARY_PREFIX%\lib
-:: add a symlink with "lib" prefixed for each lib file
-FOR /F %%G IN ('dir "*webp*lib" /B') DO (MKLINK lib%%G %%G || echo failed to link %%G )
-popd
-pushd %LIBRARY_PREFIX%\bin
-FOR /F %%G IN ('dir "*webp*dll" /B') DO (MKLINK lib%%G %%G || echo failed to link %%G )
-popd
+cmake --install . --prefix %LIBRARY_PREFIX%
+if %ERRORLEVEL% neq 0 exit 1
+
+:: Error free exit.
+echo "Error free exit!"
+exit 0
